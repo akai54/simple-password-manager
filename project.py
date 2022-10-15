433fc286f4d0f08923d4b61fc4ef35a629cf72ea
beshoux <beshoux@outlook.fr>
2022-10-15 18:23:59 +0200

    modify_entry functions correctly.

--- project.py
+++ project.py
@@ -96,14 +96,34 @@ def dec_file_cbc(key, filename, rand_num):
 
 def to_do(db_name):
     print("Please press:\n"
-          "1.Modify an entry.\n"
-          "2.Delete an entry.\n"
-          "3.Add an entry.\n"
-          "4.Show all entries.\n"
-          "5.Exit.\n")
-    choix = input()
+            "1.Modify an entry.\n"
+            "2.Delete an entry.\n"
+            "3.Add an entry.\n"
+            "4.Show all entries.\n"
+            "5.Exit.\n")
+    choix = input('choice: ')
     if choix == '1':
-        pass 
+        query_n = input('Please type the entry number: ')
+        print('1.Modify site name.\n'
+                '2.Modify username.\n'
+                '3.Modify password.\n')
+        to_change = input('choice: ')
+        if to_change == '1':
+            new_site = input('Enter new site name: ')
+            query = """UPDATE passwords SET site_name = ? WHERE rowid = ?"""
+            data = (new_site, query_n)
+            modify_entry('eli.db', query, data)
+        elif to_change == '2':
+            new_user = input('Enter new user name: ')
+            query = """UPDATE passwords SET username = ? WHERE rowid = ?"""
+            data = (new_user, query_n)
+            modify_entry('eli.db', query, data)
+        elif to_change == '3':
+            new_pwd = input('Enter new password: ')
+            query = """UPDATE passwords SET pwd = ? WHERE rowid = ?"""
+            data = (new_pwd, query_n)
+            modify_entry('eli.db', query, data)
+
     elif choix == '2':
         to_delete = input("Please type the number of the entry to delete:")
         print('\n')
@@ -117,13 +137,13 @@ def to_do(db_name):
     elif choix == '4':
         show_db(db_name)
     elif choix == '5':
+        os.system('cls' if os.name == 'nt' else 'clear')
         exit()
     else:
         print("Please choose only between the available choices.\n")
 
 
-def modify_entry(db_name, entry):
-    print(entry)
+def modify_entry(db_name, query, entry):
     # Connect to database.
     conn = sqlite3.connect(db_name)
 
@@ -131,13 +151,13 @@ def modify_entry(db_name, entry):
     cursor = conn.cursor()
 
     # Update Records. 
-    cursor.execute("UPDATE passwords SET ? = ? WHERE rowid = ?", entry)
+    cursor.execute(query, entry)
 
     # Commit our command.
     conn.commit()
     # Close our connection. 
     conn.close()
-    
+
     to_do(db_name)
 
 def delete_entry(db_name, rowid):
@@ -205,7 +225,7 @@ def load_database():
     for i, val in enumerate(list_databases):
         print(i, ",", val)
     chosen_file = int(input("\nEnter the database file number.\n"))
-    print(f'You have chosen {list_databases[chosen_file]}.\n')
+    print(f'You have chosen {list_databases[chosen_file]}\n')
     to_do(list_databases[chosen_file])
 
 def new_database():
@@ -242,6 +262,7 @@ def choice():
     elif choice == "l":
         load_database()
     elif choice == "q":
+        os.system('cls' if os.name == 'nt' else 'clear')
         exit()
     else:
         print("Please choose only between the available choices.\n")
