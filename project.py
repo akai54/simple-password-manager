04aca26250b60d6c43005083eeb9ad7695e81b10
beshoux <beshoux@outlook.fr>
2022-10-22 00:22:29 +0200

    Second version works with load_database.

--- project.py
+++ project.py
@@ -4,7 +4,264 @@ import copy
 from random import randint
 from tabulate import tabulate
 
-# sbox = [12, 5, 6, 11, 9, 0, 10, 13, 3, 14, 15, 8, 4, 7, 1, 2]
+sbox = [
+    180,
+    9,
+    32,
+    0,
+    69,
+    223,
+    248,
+    151,
+    35,
+    209,
+    60,
+    254,
+    11,
+    170,
+    185,
+    178,
+    8,
+    153,
+    14,
+    157,
+    103,
+    122,
+    176,
+    201,
+    142,
+    61,
+    77,
+    25,
+    109,
+    62,
+    54,
+    66,
+    168,
+    137,
+    193,
+    224,
+    27,
+    206,
+    20,
+    233,
+    235,
+    51,
+    117,
+    231,
+    52,
+    128,
+    252,
+    96,
+    119,
+    79,
+    220,
+    55,
+    245,
+    101,
+    183,
+    99,
+    171,
+    5,
+    41,
+    13,
+    236,
+    28,
+    95,
+    59,
+    172,
+    125,
+    57,
+    2,
+    83,
+    102,
+    179,
+    120,
+    232,
+    155,
+    123,
+    58,
+    129,
+    108,
+    225,
+    18,
+    241,
+    71,
+    243,
+    204,
+    48,
+    208,
+    106,
+    104,
+    187,
+    78,
+    26,
+    199,
+    121,
+    255,
+    132,
+    94,
+    43,
+    196,
+    197,
+    50,
+    140,
+    194,
+    186,
+    84,
+    63,
+    234,
+    23,
+    118,
+    82,
+    160,
+    1,
+    21,
+    92,
+    114,
+    15,
+    226,
+    152,
+    188,
+    80,
+    250,
+    45,
+    207,
+    205,
+    200,
+    113,
+    217,
+    216,
+    81,
+    68,
+    86,
+    91,
+    149,
+    163,
+    139,
+    70,
+    97,
+    145,
+    88,
+    195,
+    253,
+    87,
+    49,
+    39,
+    150,
+    110,
+    249,
+    173,
+    244,
+    37,
+    85,
+    73,
+    22,
+    53,
+    17,
+    6,
+    177,
+    141,
+    165,
+    44,
+    3,
+    246,
+    36,
+    159,
+    158,
+    89,
+    229,
+    16,
+    100,
+    65,
+    218,
+    56,
+    136,
+    184,
+    169,
+    143,
+    251,
+    4,
+    47,
+    181,
+    219,
+    210,
+    138,
+    127,
+    40,
+    198,
+    12,
+    164,
+    107,
+    105,
+    228,
+    90,
+    174,
+    175,
+    98,
+    115,
+    214,
+    215,
+    202,
+    30,
+    33,
+    76,
+    42,
+    156,
+    147,
+    64,
+    162,
+    154,
+    130,
+    237,
+    167,
+    72,
+    134,
+    112,
+    93,
+    222,
+    67,
+    230,
+    75,
+    192,
+    124,
+    31,
+    116,
+    131,
+    34,
+    111,
+    74,
+    46,
+    7,
+    190,
+    24,
+    182,
+    133,
+    126,
+    148,
+    19,
+    161,
+    211,
+    135,
+    238,
+    239,
+    212,
+    203,
+    247,
+    242,
+    166,
+    144,
+    221,
+    227,
+    189,
+    38,
+    213,
+    29,
+    240,
+    10,
+    146,
+    191,
+]
 
 
 def round(key, msg):
@@ -18,7 +275,7 @@ def enc(key, msg):
     return res
 
 
-# xobs = [sbox.index(n) for n in range(0, 16)]
+# xobs = [sbox.index(n) for n in range(0, 256)]
 
 
 def back_round(k, c):
@@ -131,53 +388,61 @@ def enter_pwd():
 
 
 def to_do(db_name, master=""):
-    print(
-        "Please press:\n"
-        "1.Modify an entry.\n"
-        "2.Delete an entry.\n"
-        "3.Add an entry.\n"
-        "4.Show all entries.\n"
-        "5.Exit.\n"
-    )
-    choix = input("choice: ")
-    if choix == "1":
-        query_n = input("Please type the entry number: ")
-        print("1.Modify site name.\n" "2.Modify username.\n" "3.Modify password.\n")
-        to_change = input("choice: ")
-        if to_change == "1":
-            new_site = input("Enter new site name: ")
-            query = """UPDATE passwords SET site_name = ? WHERE rowid = ?"""
-            data = (new_site, query_n)
-            modify_entry(db_name, query, data)
-        elif to_change == "2":
-            new_user = input("Enter new user name: ")
-            query = """UPDATE passwords SET username = ? WHERE rowid = ?"""
-            data = (new_user, query_n)
-            modify_entry(db_name, query, data)
-        elif to_change == "3":
-            new_pwd = input("Enter new password: ")
-            query = """UPDATE passwords SET pwd = ? WHERE rowid = ?"""
-            data = (new_pwd, query_n)
-            modify_entry(db_name, query, data)
+    try:
+        print(
+            "Please press:\n"
+            "1.Modify an entry.\n"
+            "2.Delete an entry.\n"
+            "3.Add an entry.\n"
+            "4.Show all entries.\n"
+            "5.Exit.\n"
+        )
+        choix = input("choice: ")
+        if choix == "1":
+            query_n = input("Please type the entry number: ")
+            print("1.Modify site name.\n" "2.Modify username.\n" "3.Modify password.\n")
+            to_change = input("choice: ")
+            if to_change == "1":
+                new_site = input("Enter new site name: ")
+                query = """UPDATE passwords SET site_name = ? WHERE rowid = ?"""
+                data = (new_site, query_n)
+                modify_entry(db_name, query, data)
+            elif to_change == "2":
+                new_user = input("Enter new user name: ")
+                query = """UPDATE passwords SET username = ? WHERE rowid = ?"""
+                data = (new_user, query_n)
+                modify_entry(db_name, query, data)
+            elif to_change == "3":
+                new_pwd = input("Enter new password: ")
+                query = """UPDATE passwords SET pwd = ? WHERE rowid = ?"""
+                data = (new_pwd, query_n)
+                modify_entry(db_name, query, data)
+            else:
+                choix = 1
+                to_do(db_name)
 
-    elif choix == "2":
-        to_delete = input("Please type the number of the entry to delete:")
-        print("\n")
-        delete_entry(db_name, to_delete)
-    elif choix == "3":
-        site = input("Site name:")
-        usr = input("Username:")
-        pwd = input("Password:")
-        print("\n")
-        add_entry(db_name, site, usr, pwd)
-    elif choix == "4":
-        show_db(db_name)
-    elif choix == "5":
-        print(get_master_pwd())
+        elif choix == "2":
+            to_delete = input("Please type the number of the entry to delete:")
+            print("\n")
+            delete_entry(db_name, to_delete)
+        elif choix == "3":
+            site = input("Site name:")
+            usr = input("Username:")
+            pwd = input("Password:")
+            print("\n")
+            add_entry(db_name, site, usr, pwd)
+        elif choix == "4":
+            show_db(db_name)
+        elif choix == "5":
+            print(get_master_pwd())
+            end_fun(get_master_pwd(), db_name)
+        else:
+            print("Please choose only between the available choices.\n")
+            to_do(db_name)
+    except KeyboardInterrupt:
         end_fun(get_master_pwd(), db_name)
-    else:
-        print("Please choose only between the available choices.\n")
-        to_do(db_name)
+    except Exception as e:
+        end_fun(get_master_pwd(), db_name)
 
 
 def modify_entry(db_name, query, entry):
@@ -263,11 +528,15 @@ def load_database():
         if file.endswith(".db"):
             list_databases.append(file)
 
+    if len(list_databases) == 0:
+        print("There are no available databses in the current directory.\n")
+        exit()
     for i, val in enumerate(list_databases):
         print(i, val)
     chosen_file = int(input("\nEnter the database file number.\n"))
     print(f"You have chosen {list_databases[chosen_file]}\n")
     master = enter_pwd()
+    set_my_pwd(master)
     dec_file_cbc(master, list_databases[chosen_file], 7)
     to_do(list_databases[chosen_file])
 
@@ -321,7 +590,6 @@ def choice():
         exit()
     else:
         print("Please choose only between the available choices.\n")
-    return choice
 
 
 if __name__ == "__main__":
