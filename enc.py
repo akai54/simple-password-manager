145b22902462c6224274ef95975b111681cd99b5
beshoux <beshoux@outlook.fr>
2022-10-25 12:33:55 +0200

    Enc and Dec with any password works correctly

--- enc.py
+++ enc.py
@@ -99,16 +99,8 @@ def dec_file_cbc(key, filename, rand_num):
     write_file(filename + "dec", res)
     """
 
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad, unpad
 
 # iv = pad(b"test", AES.block_size)
-
-
-def write_file(file, cont):
-    f = open(file, "wb")
-    f.write(bytes(cont))
-    f.close()
 
 
 """def encrypt(key, plaintext):
@@ -153,3 +145,55 @@ def dec_file_cbc(key_user, filename):
 # enc_file_cbc("secret", "test.db")
 # dec_file_cbc("secret", "test.txt")
 """
+import base64
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
+
+
+def write_file(file, cont):
+    f = open(file, "wb")
+    f.write(bytes(cont))
+    f.close()
+
+
+# key generation
+# key = Fernet.generate_key()
+mysalt = b"\x84\x8d\xc4\xfd\xe0\x176\xb2\xbf\x9f<v<e\xf9\xe6"
+
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256,
+    length=32,
+    salt=mysalt,
+    iterations=100000,
+    backend=default_backend(),
+)
+
+# key_2 = base64.urlsafe_b64encode(kdf.derive(key))
+
+
+def enc_fernet(key, filename):
+    with open(filename, "rb") as f:
+        original = f.read()
+    password = key.encode()
+    key_2 = base64.urlsafe_b64encode(kdf.derive(password))
+    fernet = Fernet(key_2)
+    encrypted = fernet.encrypt(original)
+    with open(filename, "wb") as encrypted_file:
+        encrypted_file.write(encrypted)
+
+
+def dec_fernet(key, filename):
+    password = key.encode()
+    key_2 = base64.urlsafe_b64encode(kdf.derive(password))
+    fernet = Fernet(key_2)
+    with open(filename, "rb") as f:
+        encrypted = f.read()
+    decrypted = fernet.decrypt(encrypted)
+    with open(filename, "wb") as decrypted_file:
+        decrypted_file.write(decrypted)
+
+
+# enc_fernet("LeonardoAAA9u05324@#@", "test.txt")
+# dec_fernet("LeonardoAAA9u05324@#@", "test.txt")
